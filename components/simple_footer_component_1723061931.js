c74343986aba0f5b771c07b0b6ac592f912f70ff
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-07 20:21:44 +0000

    Code generation

--- components/simple_footer_component_1723061931.js
+++ components/simple_footer_component_1723061931.js
@@ -2,18 +2,17 @@
 */
 Vue.component("simple_footer_component_1723061931", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-white dark:bg-gray-800">
-        <div class="grid">
-            <div class="flex-1">
-                <nav id="navbar" class="bg-white p-4 shadow-lg">
-                    <ul class="flex justify-center space-x-4">
-                        <li><a href="#home" class="text-black hover:text-purple-500">Home</a></li>
-                        <li><a href="#about" class="text-black hover:text-purple-500">About</a></li>
-                        <li><a href="#contact" class="text-black hover:text-purple-500">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
+    <footer id="footer-section" class="flex-1 bg-white dark:bg-gray-800 flex">
+        <div class="flex-1">
+            <nav id="sidebar" class="bg-white p-4 shadow-lg h-full">
+                <ul class="space-y-4">
+                    <li><a href="#home" class="text-black hover:text-purple-500">Home</a></li>
+                    <li><a href="#about" class="text-black hover:text-purple-500">About</a></li>
+                    <li><a href="#contact" class="text-black hover:text-purple-500">Contact</a></li>
+                </ul>
+            </nav>
         </div>
+        <div class="flex-1">
             <div id="footer-container" class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
                 <hr id="footer-divider" class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
                 <div id="footer-content" class="text-center">
@@ -30,7 +29,8 @@ Vue.component("simple_footer_component_1723061931", {
                     </div>
                 </div>
             </div>
-        </footer> 
+        </div>
+    </footer> 
     `,
     data() {
         return {
